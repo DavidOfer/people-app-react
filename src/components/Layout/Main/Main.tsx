b1bd0fe53082e3b01e02d6bb548e28b9fd15bb29
DavidOfer <oferd90@gmail.com>
2022-02-13 16:12:37 +0200

    added rows per page, fixed css

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { Person } from "../../../models/PersonModel";
+import { Order } from "../../../types";
 import PaginationTable from "../../PaginationTable/PaginationTable";
 import { StyledMain } from "./Main.styled";
 
+
 const Main: React.FC = () => {
-    const URL = process.env.REACT_APP_TABLE_URL +'?page='
-    const [isLoading,setIsloading] = useState(false);
+    const URL = process.env.REACT_APP_TABLE_URL + '?page='
+    const [error, setError] = useState<{ message: string } | undefined>(undefined);
+    const [isLoading, setIsloading] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
-    const [sortBy, setSortBy] = useState<{ field: String, order: String }>({ field: 'idNumber', order: 'asc' })
+    const [sortBy, setSortBy] = useState<{ field: string, order: Order }>({ field: 'idNumber', order: 'asc' })
+    const [rowsPerPage, setRowsPerPage] = useState(10);
     const sortByHandler = (field: string) => {
         setSortBy((prevSortBy) => {
             if (prevSortBy.field !== field) {
@@ -18,45 +22,43 @@ const Main: React.FC = () => {
             }
             return { field, order: 'asc' }
         })
-        setCurrentPage(prevPage=>0)
+        setCurrentPage(prevPage => 0)
     }
-    // const onNext = () => {
-    //     setCurrentPage(prevPage => prevPage + 1)
-    // }
-    // const onPrevious = () => {
-    //     setCurrentPage(prevPage => prevPage - 1)
-    // }
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setCurrentPage(prevPage => 0);
+    };
+
     const handleChangePage = (event: unknown, newPage: number) => {
-        console.log('clicked change page')
-        console.log(newPage)
         setCurrentPage(newPage);
     };
-    const [data, setData] = useState<{count:number,people:Person[]}>({count:0,people:[]})
+
+    const [data, setData] = useState<{ count: number, people: Person[] }>({ count: 0, people: [] })
     useEffect(() => {
-        setIsloading(true)
-        fetch(URL + currentPage + `&sortBy=${sortBy.field}&order=${sortBy.order}`)
+        setIsloading(true);
+        setError(undefined);
+        fetch(URL + currentPage + `&sortBy=${sortBy.field}&order=${sortBy.order}&perPage=${rowsPerPage}`)
             .then(result => result.json())
             .then(data => {
-                setData({count:data.count,people:data.result})
+                setData({ count: data.count, people: data.result })
                 setIsloading(false);
                 console.log(data)
                 console.log(URL + currentPage + `&sortBy=${sortBy.field}&order=${sortBy.order}`)
             })
-            .catch(err=>{
+            .catch(err => {
                 setIsloading(false);
-                console.log(err);
+                const message = err.message ? err.message : 'An error has occured while attempting to fetch the data';
+                setError({ message: message })
                 console.log(URL + currentPage + `&sortBy=${sortBy.field}&order=${sortBy.order}`)
             })
-    }, [currentPage, sortBy,URL])
+    }, [currentPage, sortBy, URL,rowsPerPage])
     return (
         <StyledMain>
-            {/* {people.map((person, index1) => <div key={index1} style={{ textAlign: 'center' }}>{Object.entries(person).map((field, index) => <div key={index}>{field[0]}: {field[1]} </div>)} </div>)} */}
-            {/* <button onClick={onNext}>next</button>
-            <button onClick={onPrevious}>previous</button> */}
-            {/* <div className="paginationContainer">
-                <PaginationControlled page={currentPage} handleChange={handleChange} />
-            </div> */}
-            <PaginationTable isLoading={isLoading} count={data.count} sortByHandler={sortByHandler} page={currentPage} rows={data.people} handleChangePage={handleChangePage} />
+            {error && error.message}
+            <PaginationTable isLoading={isLoading} count={data.count} sortBy={sortBy}
+                sortByHandler={sortByHandler} page={currentPage} rows={data.people} rowsPerPage={rowsPerPage}
+                handleChangePage={handleChangePage} handleChangeRowsPerPage={handleChangeRowsPerPage} />
         </StyledMain>
     );
 }
--- src/components/PaginationTable/PaginationTable.tsx
+++ src/components/PaginationTable/PaginationTable.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,65 +9,102 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@mui/styles';
 import { Person } from '../../models/PersonModel';
+import { TableSortLabel } from '@mui/material';
+import { Order } from '../../types';
 
 const useStyles = makeStyles({
-  paper:{
+  paper: {
     position: "relative",
   },
   tableBody: {
-      '& > tr:nth-child(odd)': {
-        backgroundColor: 'rgb(200,200,200)',
+    '& > tr:nth-child(odd)': {
+      backgroundColor: 'rgb(200,200,200)',
     }
   },
-  loading:{
+  loading: {
     backgroundColor: 'rgba(0, 0, 0, 0.75)',
     position: 'absolute',
     width: '100%',
     height: '100%',
-    display:'flex',
-    justifyContent:'center',
-    alignContent:'center',
-    alignItems:'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
     '& > h3': {
-      display:'inline-block',
-      height:'30px',
-      width:'250px',
-      color:'white'
-  }
+      display: 'inline-block',
+      color: 'white'
+    }
   }
 });
 
+interface HeadCell {
+  id: keyof Person;
+  label: string;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'idNumber',
+    label: 'ID',
+  },
+  {
+    id: 'firstName',
+    label: 'First Name',
+  },
+  {
+    id: 'lastName',
+    label: 'Last Name',
+  },
+  {
+    id: 'gender',
+    label: 'Gender',
+  },
+  {
+    id: 'birthDate',
+    label: 'Birth Date',
+  }
+];
 
 interface TableProps {
-  isLoading:boolean,
-  count:number,
-  rows: Person[],
-  page: number
-  handleChangePage: (event: unknown, newPage: number) => void,
-  sortByHandler:(field:string)=>void,
+  isLoading: boolean;
+  count: number;
+  rows: Person[];
+  page: number;
+  sortBy: { field: string, order: Order };
+  rowsPerPage:number;
+  handleChangePage: (event: unknown, newPage: number) => void;
+  sortByHandler: (field: string) => void;
+  handleChangeRowsPerPage: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 
-const PaginationTable: React.FC<TableProps> = ({ isLoading,count, rows, page, sortByHandler,handleChangePage}) => {
-  const classes = useStyles();
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-  };
+const PaginationTable: React.FC<TableProps> = ({
+  isLoading, count, rows, page, sortBy, rowsPerPage, sortByHandler, handleChangePage,
+  handleChangeRowsPerPage
+}) => {
+  const classes = useStyles();
+
   const emptyRows =
     rowsPerPage - rows.length;
 
   return (
     <TableContainer component={Paper} className={classes.paper}>
-                {isLoading && <div className={classes.loading}><h3>Loading...(1s intentional delay)</h3></div>}
+      {isLoading && <div className={classes.loading}><h3>Loading...</h3></div>}
       <Table size="small" aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell onClick={e=>sortByHandler('idNumber')}>ID</TableCell>
-            <TableCell onClick={e=>sortByHandler('firstName')}>First Name</TableCell>
-            <TableCell onClick={e=>sortByHandler('lastName')}>Last Name</TableCell>
-            <TableCell onClick={e=>sortByHandler('gender')}>Gender</TableCell>
-            <TableCell onClick={e=>sortByHandler('birthDate')}>Birth Date</TableCell>
+            {headCells.map(headCell =>
+              <TableCell key={headCell.id} sortDirection={sortBy.field === headCell.id ? sortBy.order : false}>
+                <TableSortLabel
+                  active={sortBy.field === headCell.id}
+                  direction={sortBy.field === headCell.id ? sortBy.order : 'asc'}
+                  onClick={e => sortByHandler(headCell.id)}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              </TableCell>
+            )}
           </TableRow>
         </TableHead>
         <TableBody className={classes.tableBody}>
@@ -85,7 +122,7 @@ const PaginationTable: React.FC<TableProps> = ({ isLoading,count, rows, page, so
               </TableRow>
             ))}
           {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
+            <TableRow style={{ height: 30 * emptyRows }}>
               <TableCell colSpan={6} />
             </TableRow>
           )}
